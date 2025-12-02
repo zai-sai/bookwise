3a3c4bb4f0e01b0672e70ce0818c300e0652b0e1
Rafi Fitzmaurice <rafi.fitzm@gmail.com>
2025-12-02 16:33:52 +0000

    Add links to shelf cards in home page

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  get "shelves/:id", to: "pages#home", as: :shelf
+  get "shelves", to: "pages#home", as: :shelves
 end
