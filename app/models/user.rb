e0dcaea05685197cd8e98d3c70bf6c004037327a
Marcus Chien <chienmarcus7@gmail.com>
2025-12-03 17:41:19 +0000

    did bookcards

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+<<<<<<< HEAD
 
-  has_many :user_books, dependent: :destroy
+=======
   has_many :books, through: :user_books
+>>>>>>> f744c165d53cd0e4127756f9d4139d7144a3aa3b
+  has_many :user_books, dependent: :destroy
   has_many :shelves
 
   after_create :create_shelves
