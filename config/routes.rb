146d78a028bdf7c2c5856444f696feff6d691336
Rafi Fitzmaurice <rafi.fitzm@gmail.com>
2025-12-03 10:53:46 +0000

    Home page working

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   get "books/:id", to: "books#show", as: :book
+
+  # to make home page work for now
+  get "shelves/:id", to: "pages#home", as: :shelf
+  get "shelves", to: "pages#home", as: :shelves
 end
