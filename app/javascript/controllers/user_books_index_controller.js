35a006312e70b8c1e8490c0a580c8c87889ffd50
Rafi Fitzmaurice <rafi.fitzm@gmail.com>
2025-12-05 12:40:53 +0000

    Add stimulus controller to toggle all/read/unread books buttons

---
+++ app/javascript/controllers/user_books_index_controller.js
@@ -0,0 +1,71 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="user-books-index"
+export default class extends Controller {
+  static targets = ["allBooks", "readBooks", "unreadBooks"];
+
+  connect() {
+    console.log("Connected to userBooksIndexController");
+
+  }
+
+  toggleButtons() {
+  }
+
+  showAllBooks() {
+    this.allBooksTarget.classList.add("active");
+    this.allBooksTarget.setAttribute("aria-pressed", "true");
+
+    this.readBooksTarget.classList.remove("active");
+    this.readBooksTarget.setAttribute("aria-pressed", "false");
+
+    this.unreadBooksTarget.classList.remove("active");
+    this.unreadBooksTarget.setAttribute("aria-pressed", "false");
+
+    console.log("All books");
+  }
+
+  toggleReadBooks() {
+    if (this.allBooksTarget.classList.contains("active")) {
+      // => All Books -> Read Books
+      this.allBooksTarget.classList.remove("active");
+      this.allBooksTarget.setAttribute("aria-pressed", "false");
+
+      this.readBooksTarget.classList.add("active");
+      this.readBooksTarget.setAttribute("aria-pressed", "true");
+
+    } else if (this.unreadBooksTarget.classList.contains("active")) {
+      // => Unread Books -> Read Books
+      this.unreadBooksTarget.classList.remove("active");
+      this.unreadBooksTarget.setAttribute("aria-pressed", "false");
+
+      this.readBooksTarget.classList.add("active");
+      this.readBooksTarget.setAttribute("aria-pressed", "true");
+    } else {
+      // => Read Books -> All Books
+      this.showAllBooks()
+    }
+  }
+
+  toggleUnreadBooks() {
+    if (this.allBooksTarget.classList.contains("active")) {
+      // => All Books -> Unread Books
+      this.allBooksTarget.classList.remove("active");
+      this.allBooksTarget.setAttribute("aria-pressed", "false");
+
+      this.unreadBooksTarget.classList.add("active");
+      this.unreadBooksTarget.setAttribute("aria-pressed", "true");
+    } else if (this.readBooksTarget.classList.contains("active")) {
+      // => Read Books -> Unread Books
+      this.readBooksTarget.classList.remove("active");
+      this.readBooksTarget.setAttribute("aria-pressed", "false");
+
+      this.unreadBooksTarget.classList.add("active");
+      this.unreadBooksTarget.setAttribute("aria-pressed", "true");
+    } else {
+      // => Unread Books -> All Books
+      this.showAllBooks()
+    }
+  }
+
+}
