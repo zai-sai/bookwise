b6a34b0aa7bb790ee280ef21a3573e608a0189a6
Zainab Saigol <z.s.saigol@hotmail.com>
2025-12-09 15:59:40 +0000

    added a small green tick to the book cover if the book has been read. Added to shelves index, shelf show, user_books index and searches index

--- app/models/shelf_book.rb
+++ app/models/shelf_book.rb
@@ -2,7 +2,7 @@ class ShelfBook < ApplicationRecord
   belongs_to :user_book
   belongs_to :shelf
 
-  delegate :book, :title, :author, :description, :image_link, :tag, to: :user_book
+  delegate :book, :title, :author, :description, :image_link, :tag, :status, to: :user_book
 
   include PgSearch::Model
 
