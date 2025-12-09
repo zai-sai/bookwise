647e0b1dbd92421309f4830a501dceff55c38150
Margaux Schreurs <schreursmargaux@hotmail.com>
2025-12-09 11:24:31 +0000

    Search bar on shelves show page

--- app/models/shelf_book.rb
+++ app/models/shelf_book.rb
@@ -3,4 +3,13 @@ class ShelfBook < ApplicationRecord
   belongs_to :shelf
 
   delegate :book, :title, :author, :description, :image_link, :tag, to: :user_book
+
+  include PgSearch::Model
+
+  pg_search_scope :search_by_title_and_author,
+  against: [:title, :author],
+  using: {
+    tsearch: { prefix: true }  # allows searching with partial words
+  }
+
 end
